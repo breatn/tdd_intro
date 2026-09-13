4c7cc20f0283b573bac10a5743b0c606f598af4c
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2026-09-13 16:31:07 +0100

    Moved to functional

--- roman.py
+++ roman.py
@@ -1,3 +1,51 @@
-def roman_to_int(roman: str) -> int:
-    pass
+import itertools as it
 
+digits = {
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1,
+}
+
+# def roman_to_int(roman: str) -> int:
+#     buffer = list(roman)
+#     values = [0]
+#
+#     while buffer:
+#         value = digits[buffer.pop()]
+#
+#         if value < values[-1]:
+#             value *= -1
+#
+#         values.append(value)
+#
+#     return sum(values)
+
+# def roman_to_int(roman: str) -> int:
+#     values = [digits[x] for x in roman]
+#     paired = zip(values, values[1:] + [0] )
+#
+#     return sum(-a if a < b else a for a, b in paired)
+
+def roman_to_int(roman: str) -> int:
+    a_value, b_value  = it.tee(
+        map(
+            lambda x: digits[x],
+            list(roman)
+        )
+    )
+
+    paired = zip(a_value,
+                 it.chain(
+                     it.islice(
+                         b_value,
+                         1,
+                         None
+                     ),
+                     [0])
+                 )
+
+    return sum(-a if a < b else a for a, b in paired)
